8e1a8d5d85f6bbb0eb74045bcd2a0a895f5b5070
venkata nagendra  bharath <74871715+bharth-stack@users.noreply.github.com>
2021-07-25 20:14:39 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,18 @@
+
+package table;
+import java .util.*;
+public class Table {
+public static void main(String[] args) {
+int i,j,n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the table number:");
+n=s.nextInt();
+for(i=1;i<=10;i++)
+{
+    System.out.println(n+"x"+i+"="+(n*i));
+            
+}
+    
+    }
+    
+}
