4092bbe84511756b53a90f346e210c90c3740ca4
venkata nagendra  bharath <74871715+bharth-stack@users.noreply.github.com>
2021-07-26 21:54:37 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+class Add
+{
+public static void main(String arg[])
+{
+int a,b,c;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the a=");
+a=s.nextInt();
+System.out.println("enter the b=");
+b=s.nextInt();
+c=a+b;
+System.out.println("add="+c);
+}
+}
