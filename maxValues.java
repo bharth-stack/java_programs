77a082ad9621d24462f94a3f2705010a76602193
venkata nagendra  bharath <74871715+bharth-stack@users.noreply.github.com>
2021-08-02 22:18:30 +0530

    Add files via upload

---
+++ maxValues.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.*;
+public class maxValues
+{
+         public static void main(String arg[])
+        {
+         Scanner sc=new Scanner(System.in);
+System.out.println("Min Value of integer:"+Integer.MIN_VALUE);
+System.out.println("Max Value of integer:"+Integer.MAX_VALUE);
+System.out.println("Min Value of integer:"+Byte.MIN_VALUE);
+System.out.println("Max Value of integer:"+Byte.MAX_VALUE);
+System.out.println("Min Value of integer:"+Short.MIN_VALUE);
+System.out.println("Max Value of integer:"+Short.MAX_VALUE);
+System.out.println("Min Value of integer:"+Long.MIN_VALUE);
+System.out.println("Max Value of integer:"+Long.MAX_VALUE);
+System.out.println("Min Value of integer:"+Float.MIN_VALUE);
+System.out.println("Max Value of integer:"+Float.MAX_VALUE);
+System.out.println("Min Value of integer:"+Double.MIN_VALUE);
+System.out.println("Max Value of integer:"+Double.MAX_VALUE);
+System.out.println("Min Value of integer:"+Character.MIN_VALUE);
+System.out.println("Max Value of integer:"+Character.MAX_VALUE);
+        }
+}
